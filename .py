501088661fcaf7cdda3b5ab060d700a84039871e
Dhanki <shrovandhanki@gmail.com>
2025-08-03 11:57:45 +0530

    Create .py

---
+++ .py
@@ -0,0 +1,12 @@
+username=input("enter the username")
+password=input("Enter the password")
+if username =='shrovan':
+    print('the username is correct')
+    if password =='18122007':
+         print('the password is correct')
+         print('successfully logged in')
+    else:
+         print('the password is incorrect')
+else:
+    print('the username is incorrect')
+
